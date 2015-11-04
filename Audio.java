83746798dbc34ce71575dc03658977bb077d83cd
Matthew Kipp <matthewkipp55@gmail.com>
2015-11-03 22:31:27 -0500

    added default code to Audio and User classes

---
+++ Audio.java
@@ -0,0 +1,13 @@
+public class Audio extends Item{
+	protected String artistName;
+	public String getInfo(...){
+		//returns sNo, Name, Artist name, etc in a string
+	}
+	@Override
+	public int getPrice(...){
+		//override
+	}
+	public int getListInfo(...){
+		//based on the value of Type(CD or MP3) print the list of Items
+	}
+}
