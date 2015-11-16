b97c107ec58686251b4fc1e9b657e7b1b19946c5
Matthew Kipp <matthewkipp55@gmail.com>
2015-11-16 00:11:19 -0500

    main class now runs UserInterface class

---
+++ HWK4_youy2.java
@@ -0,0 +1,16 @@
+//The party. Starts. Here.
+//Also where Matt types things wrong and fixes them later
+
+public class HWK4_youy2 {
+
+	public static void main(String[] args) {
+
+		UserInterface ui = new UserInterface();
+
+		System.out.println("The Ultimate Shopping Experience Begins. NOW");
+		System.out.println("Just roll with it.");
+
+		ui.mainLoop();
+
+	}
+}
